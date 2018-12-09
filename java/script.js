ba27579414622e884786578afe0ca82c689001a3
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2018-12-09 13:18:44 +0100

    Horizontal scrool enabled

--- java/script.js
+++ java/script.js
@@ -1,3 +1,9 @@
-videoElement.addEventListener('canplaythrough',function(){
-  this.play();
-});
+$(document).ready(function(){
+
+      // Script for horizontal scroll on mousewheel //
+    $('.all-content').mousewheel(function(e, delta) {
+      this.scrollLeft -= (delta * 1000); e.preventDefault();
+    });
+
+
+  });
